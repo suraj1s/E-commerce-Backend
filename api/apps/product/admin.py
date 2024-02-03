1287944db3c3239101da1400a5ff608b9d16475f
suraj subedi <surajsubedi059@gmail.com>
2024-02-03 21:41:10 +0545

    fix : fixed model registration in admin site

--- api/apps/product/admin.py
+++ api/apps/product/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Product , Brand , Category
+from .models import Product, Brand, Category
 
-admin.site.register(Product , Brand , Category)
+admin.site.register(Product)
+admin.site.register(Brand)
+admin.site.register(Category)
