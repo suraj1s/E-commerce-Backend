f21da4db2fc69c741d2c959797553985940346e9
suraj subedi <surajsubedi059@gmail.com>
2024-02-03 21:19:53 +0545

    created product model

--- api/apps/product/models.py
+++ api/apps/product/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from api.apps.common.models import BaseModel
 
-# Create your models here.
+sizes = (
+    ('S', 'Small'),
+    ('M', 'Medium'),
+    ('L', 'Large'),
+    ('XL', 'Extra Large'),
+    ('XXL', 'Extra Extra Large'),
+)
+
+color = (
+    ('Red', 'Red'),
+    ('Blue', 'Blue'),
+    ('Green', 'Green'),
+    ('Black', 'Black'),
+    ('White', 'White'),
+)
+
+
+class Product(BaseModel):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+    deleted = models.BooleanField(default=True)
+    discountPercentage = models.DecimalField(decimal_places=2, max_digits=3, default=0.00)
+    rating = models.IntegerField(default=0 , max_value=5)
+    stock = models.IntegerField(default=0)
+   
+    brand = models.ForeignKey("Brand", on_delete=models.CASCADE)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    # thumbnail = models.ImageField(upload_to="products/thumbnails", null=True, blank=True)
+    # images = models.ImageField(upload_to="products/images", null=True, blank=True)
+    color = models.CharField(max_length=120, choices=color, null= True, blank=True)
+    size = models.CharField(max_length=120, choices=sizes ,null= True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+class Brand(BaseModel):
+    name = models.CharField(max_length=120)
+    lable = models.CharField(max_length=120)
+    description = models.TextField()
+    
+
+    def __str__(self):
+        return self.name
+
+class Category(BaseModel):
+    name = models.CharField(max_length=120)
+    lable = models.CharField(max_length=120)
+    description = models.TextField()
+   
+    def __str__(self):
+        return self.name
--- readme.md
+++ readme.md
@@ -22,8 +22,9 @@ python manage.py runserver
 # Output the list of installed packages and their versions to a file
 pip3 freeze > requirements.txt
 
-Install the packages listed in the requirements.txt file
+# Install the packages listed in the requirements.txt file
 pip3 install -r requirements.txt
 
+# run docker in detachmode with prot 6379 in both local and docker container with redis database
 docker run -d --rm -p 6379:6379 redis
 ```
