a48e4f04bde78969847b4b961f687149e19135de
suraj subedi <surajsubedi059@gmail.com>
2024-02-14 22:03:22 +0545

    cart api integarting on cart page

--- api/apps/cart/serializers.py
+++ api/apps/cart/serializers.py
@@ -2,11 +2,24 @@ from rest_framework import serializers
 from .models import Cart
 
 class CartSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField()
+    # user = serializzers.StringRelatedField()
+    product = serializers.SerializerMethodField()
     class Meta:
         model = Cart
-        fields = '__all__'
+        fields = [ "id" , "product" , "quantity" ]
+        
+
 
     def get_user(self, obj):
         request = self.context.get('request')
         return request.user
+
+    def get_product(self, obj):
+        product = {
+            'id': obj.product.id,
+            'title': obj.product.title,
+            'price': obj.product.price,
+            'discount': obj.product.discountPercentage,
+            'thumbnail': obj.product.thumbnail,
+            }
+        return product
