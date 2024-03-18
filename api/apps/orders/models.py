5c797fc46a3ca637427445293572d5aa0a378d09
suraj subedi <surajsubedi059@gmail.com>
2024-03-18 23:14:25 +0545

    order working

--- api/apps/orders/models.py
+++ api/apps/orders/models.py
@@ -14,39 +14,6 @@ class OrderEnum(models.TextChoices):
     HOLD = 'hold'
 
 
-# for this serilizer create the order and make the orderstatus checkedout
-# class CheckoutCreateSerializer(serializers.ModelSerializer):
-#     user = serializers.HiddenField(
-#         default=serializers.CurrentUserDefault()
-#     )
-#     checkout_items = serializers.ListField(write_only=True, required=False)
-
-#     class Meta:
-#         model = Checkout
-#         fields = ["id", "checkout_items", "address", "payment", "user"]
-
-#     def create(self, validated_data):
-#         checkout_items_data = validated_data.pop('checkout_items', None)
-#         checkout = Checkout.objects.create(**validated_data)
-        
-#         if checkout_items_data:
-#             for cart_id in checkout_items_data:
-#                 cart = Cart.objects.get(id=cart_id)
-#                 checkout_item = CheckoutItems.objects.create(
-#                     product=cart.product,
-#                     quantity=cart.quantity
-#                 )
-#                 checkout.checkout_items.add(checkout_item)
-#                 cart.delete()
-#         # at this point create a order with orderstatus as pending
-#         order = Order.objects.create(
-#             user=validated_data['user'],
-#             order_status='pending'
-#         )
-#         order.order_detail = checkout
-#         order.save()
-#         return checkout
-
 class Order (BaseModel):
     order_status = models.CharField(max_length=255, choices=OrderEnum.choices, default=OrderEnum.PENDING)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- api/apps/orders/serializers.py
+++ api/apps/orders/serializers.py
@@ -1,11 +1,67 @@
 from rest_framework import serializers
 from .models import Order
+from api.apps.checkout.models import Checkout, CheckoutItems
+
+
+class CheckoutItemsSerializer(serializers.ModelSerializer):
+    product = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CheckoutItems
+        fields = ["id", "product", "quantity"]
+
+    def get_product(self, obj):
+        return {
+            'id': obj.product.id,
+            'title': obj.product.title,
+            'price': obj.product.price,
+            'discount': obj.product.discountPercentage,
+            'thumbnail': obj.product.thumbnail,
+        }
+
+class CheckoutGetSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(
+        default=serializers.CurrentUserDefault()
+    )
+    checkout_items = CheckoutItemsSerializer(many=True)
+    class Meta:
+        model = Checkout
+        fields = ["id", "checkout_items", "address", "payment", "user"]
+
 
 class OrderSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(
         default=serializers.CurrentUserDefault()
     )
+    order_detail = CheckoutGetSerializer()  # Instantiate the serializer class
     class Meta:
         model = Order
-        fields = "__all__"
+        fields = ["id", "order_status", "order_detail", "user"]
+
+
+# for these modal
+
+# class Order (BaseModel):
+#     order_status = models.CharField(max_length=255, choices=OrderEnum.choices, default=OrderEnum.PENDING)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     order_detail = models.ForeignKey(Checkout, on_delete=models.CASCADE)
+#     def __str__(self):
+#         return f'{self.user}  - {self.order_status}'
+
+# class CheckoutItems (BaseModel):
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     quantity = models.PositiveIntegerField(default=1)
+
+#     def __str__(self):
+#         return f'{self.product} - {self.quantity}'
+
+# class Checkout(BaseModel):
+#     address = models.ForeignKey(Address, on_delete=models.CASCADE)
+#     payment = models.ForeignKey(Payment, on_delete=models.CASCADE, null=True, blank=True)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     checkout_items = models.ManyToManyField(CheckoutItems)
+#     def __str__(self):
+#         return f'{self.user} - {self.payment} - {self.address}'
+
+
 
