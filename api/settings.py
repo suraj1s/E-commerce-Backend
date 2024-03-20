74a74089e5e8ed7e859394c98fc2b55e07063210
suraj subedi <surajsubedi059@gmail.com>
2024-03-20 21:59:45 +0545

    cleaned migrations

--- api/settings.py
+++ api/settings.py
@@ -86,21 +86,21 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # # Database
 # # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Replace the SQLite DATABASES configuration with PostgreSQL:
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         # Replace this value with your local database's connection string.
-#         default=os.environ.get("DATABASE_URL"),
-#         conn_max_age=600
-#     )
-# }
+DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600
+    )
+}
 
 
 # Password validation
